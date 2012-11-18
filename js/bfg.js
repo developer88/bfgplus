b3e045a3fe6a0e114a3c52c384ca565b4ee2c8fc
Andrey Eremin <dsoft88@gmail.com>
2012-11-18 21:28:46 +0400

    Added i18n internalization.

--- js/bfg.js
+++ js/bfg.js
@@ -1,5 +1,11 @@
-function Bfg() {
+function Bfg (options) {
+    this.t = null;
+    this.options = options;
     this.initialize = function() {
-        // do some staff
+       var option = { resGetPath: 'js/locales/__lng__.json', lng: 'en-US' };
+       // TODO delete lng option in the future
+       this.t = i18n.init(option, function(t) { 
+          //var appName = t("key");
+        });
     };
 }
