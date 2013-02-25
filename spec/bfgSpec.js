e0ae656e4fb4c6dd29136d312cc9000117f817f3
Eremin Andrey <dsoft88@gmail.com>
2013-02-25 21:35:14 +0400

    Working on spec.

---
+++ spec/bfgSpec.js
@@ -0,0 +1,40 @@
+describe("Initialize Bfg Plus", function() {
+
+  it("with appropriate parameters will not raise an error", function() {
+
+  });
+
+  it("with inappropriate parameters will raise an error", function() {
+
+  });
+
+  it("with appropriate parameters but wrong api key will show 'No post' message", function() {
+
+  });
+
+});
+
+
+describe("Initialize BFG Plus with parameter", function() {
+
+  var options = {};
+
+  beforeEach(function() {
+    options = {
+      dom:'#bfg-sample',
+      api:'AIzaSyCQ0r-Z6wsYLFEbCGfJEzmrMuc4fc2BWdw',
+      user:'102960425588359758591'
+    };
+  });      
+
+  it("count = 50 will render 50 posts from Google+", function() {
+    options['count'] = 50
+
+  });
+
+  it("locale = 'ru-RU' will render 100 posts with Russian locale", function() {
+    options['locale'] = 'ru-RU'
+
+  });
+
+});
